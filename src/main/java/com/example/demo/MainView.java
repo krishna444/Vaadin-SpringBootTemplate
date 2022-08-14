62f0082926ba932bcbc1b170610d216e264b8127
Krishna@Lenovo-Window10 <krishna444@gmail.com>
2022-08-14 22:34:53 +0200

    Code optimizations.

--- src/main/java/com/example/demo/MainView.java
+++ src/main/java/com/example/demo/MainView.java
@@ -8,46 +8,42 @@ import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.server.PWA;
-import com.vaadin.flow.theme.Theme;
-import com.vaadin.flow.theme.lumo.Lumo;
 
 
 @PageTitle("Main")
 @Route(value = "")
-class MainView extends VerticalLayout{
-    private int rubinCount=0,babaCount=0,mummyCount=0;
-    public MainView(){
+class MainView extends VerticalLayout {
+    private int rubinCount = 0, babaCount = 0, mummyCount = 0;
 
-        Button button=new Button("Click here");
-        HorizontalLayout hl=new HorizontalLayout();
-        Label rubinLabel=new Label("Rubin");
-        TextField textFieldRubin=new TextField("");
-        Label babaLabel=new Label("Baba");
-        TextField textFieldBaba=new TextField("");
-        Label mummyLabel=new Label("Mummy");
-        TextField textFieldMummy=new TextField("");
+    public MainView() {
+
+        Button button = new Button("Click here");
+        HorizontalLayout hl = new HorizontalLayout();
+        Label rubinLabel = new Label("Rubin");
+        TextField textFieldRubin = new TextField("");
+        Label babaLabel = new Label("Baba");
+        TextField textFieldBaba = new TextField("");
+        Label mummyLabel = new Label("Mummy");
+        TextField textFieldMummy = new TextField("");
 
 
-
-        TextArea textArea=new TextArea("");
-        button.addClickListener(click->textArea.setLabel("Rubin Paudel"));
-        textArea.addKeyDownListener(keydown-> {
-            int i=(int)(Math.random()*100);
-            rubinCount=i>67?rubinCount+1:rubinCount;
-            babaCount=i>33 && i<=67?babaCount+1:babaCount;
-            mummyCount=i<=33?mummyCount+1:mummyCount;
-            textFieldRubin.setValue(rubinCount+"");
-            textFieldBaba.setValue(babaCount+"");
-            textFieldMummy.setValue(mummyCount+"");
-            String text=i>67?"Rubin Paudel":i>33 && i<=67?"Krishna Paudel":"Mummy";
-            textArea.setValue(textArea.getValue() + "\n"+text);
-            i++;
-        }
+        TextArea textArea = new TextArea("");
+        button.addClickListener(click -> textArea.setLabel("Rubin Paudel"));
+        textArea.addKeyDownListener(keydown -> {
+                    int i = (int) (Math.random() * 100);
+                    rubinCount = i > 67 ? rubinCount + 1 : rubinCount;
+                    babaCount = i > 33 && i <= 67 ? babaCount + 1 : babaCount;
+                    mummyCount = i <= 33 ? mummyCount + 1 : mummyCount;
+                    textFieldRubin.setValue(rubinCount + "");
+                    textFieldBaba.setValue(babaCount + "");
+                    textFieldMummy.setValue(mummyCount + "");
+                    String text = i > 67 ? "Rubin Paudel" : i > 33 && i <= 67 ? "Krishna Paudel" : "Mummy";
+                    textArea.setValue(textArea.getValue() + "\n" + text);
+                    i++;
+                }
         );
-        this.add(button,rubinLabel,textFieldRubin,babaLabel,textFieldBaba,mummyLabel,textFieldMummy,textArea);
+        this.add(button, rubinLabel, textFieldRubin, babaLabel, textFieldBaba, mummyLabel, textFieldMummy, textArea);
     }
-
 
 
 }
